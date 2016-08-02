7c29b3b714b6a202ba8f49cf7f0dbb0fd93ebc35
Robert Deutz <rdeutz@googlemail.com>
2016-08-02 13:28:30 +0200

    mover code into model so that it works from BE

--- source/administrator/components/com_volunteers/models/volunteers.php
+++ source/administrator/components/com_volunteers/models/volunteers.php
@@ -52,6 +52,26 @@ class VolunteersModelVolunteers extends VolunteersModelBase
 		return $query;
 	}
 	
+	
+	/**
+	 * Allows data and form manipulation after preprocessing the form
+	 *
+	 * @param   FOFForm  $form    A FOFForm object.
+	 * @param   array    &$data   The data expected for the form.
+	 * @codeCoverageIgnore
+	 *
+	 * @return  void
+	 */
+	public function onAfterPreprocessForm(FOFForm &$form, &$data)
+	{
+		if (FOFPlatform::getInstance()->isFrontend())
+		{
+			$form->removeField('slug');
+			$form->removeField('enabled');
+			$form->removeField('notes');
+		}
+	}
+	
 	/**
 	 * This method runs after an item has been gotten from the database in a read
 	 * operation. You can modify it before it's returned to the MVC triad for
@@ -71,4 +91,95 @@ class VolunteersModelVolunteers extends VolunteersModelBase
 			$record->honorroll = $this->getVolunteerHonourroll($record->volunteers_volunteer_id);
 		}
 	}
+	
+	/**
+	 * This method runs before the $data is saved to the $table. Return false to
+	 * stop saving.
+	 *
+	 * @param   array     &$data   The data to save
+	 * @param   FOFTable  &$table  The table to save the data to
+	 *
+	 * @return  boolean  Return false to prevent saving, true to allow it
+	 */
+	protected function onBeforeSave(&$data, &$table)
+	{
+		if ($data['removeimage'] == 0)
+		{
+			// Grab the old value so that it doesn't get lost
+			$data['image'] = $table->image;
+		}
+
+		$result = parent::onBeforeSave($data, $table);
+		
+		if (! $result)
+		{
+			return false;
+		}
+		
+		$files = JFactory::getApplication()->input->files;
+		
+		$file = $files->get('image');
+		
+		if ($file['error'] == 0)
+		{
+			$rand 		= substr(md5(microtime()),rand(0,26),4);
+			$firstname 	= $data['firstname'];
+			$lastname 	= $data['lastname'];
+			$name 		= JFilterOutput::stringURLSafe($firstname.'-'.$lastname);
+			$name		= $rand.'_'.$name;
+			
+			// Filetype
+			$filename = ($file['name']);
+			$filetype = strstr($filename, '.');
+			$filename = $name.$filetype;
+			
+			// Path
+			$filepath = JPath::clean(JPATH_SITE.'/images/volunteers/' . $filename);
+			
+			// Do the upload
+			jimport('joomla.filesystem.file');
+			if (!JFile::upload($file['tmp_name'], $filepath))
+			{
+				$this->setError(JText::_('Upload file error'));
+				return false;
+			}
+			
+			// Instantiate our JImage object
+			$image = new JImage($filepath);
+			
+			$sizes = array('250x250', '50x50');
+			$image->createThumbs($sizes, JImage::CROP_RESIZE);
+			
+			// Set image url
+			$data['image'] = $filename;
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * This method runs after the data is saved to the $table and syncs the user email address from the volunteers table
+	 * to the users table
+	 *
+	 * @param   FOFTable  &$table  The table which was saved
+	 *
+	 * @return  boolean
+	 */
+	protected function onAfterSave(&$table)
+	{
+		$result = parent::onAfterSave($table);
+		
+		// Sync email with user table
+		$user = JFactory::getUser($table->user_id);
+		
+		if ($user->email != $table->email)
+		{
+			$user->email = $table->email;
+			
+			$result &= $user->save();
+		}
+		
+		return $result;
+	}
+	
 }
--- source/components/com_volunteers/controllers/volunteers.php
+++ source/components/com_volunteers/controllers/volunteers.php
@@ -31,82 +31,8 @@ class VolunteersControllerVolunteers extends FOFController
 	 */
 	public function onAfterSave()
 	{
-		// Get volunteer data
-		$volunteer = $this->getThisModel()->getItem();
-
-		// Update email
-		if($volunteer->email)
-		{
-			$db 		= JFactory::getDbo();
-			$query 		= $db->getQuery(true);
-			$fields 	= array($db->quoteName('email') . ' = ' . $db->quote($volunteer->email));
-			$conditions = array($db->quoteName('id') . ' = ' . $db->quote($volunteer->user_id));
-			$query->update($db->quoteName('#__users'))->set($fields)->where($conditions);
-			$db->setQuery($query);
-			$result = $db->execute();
-		}
-
 		// Redirect
 		$this->setRedirect(JRoute::_('index.php?option=com_volunteers&view=my'), JText::_('COM_VOLUNTEERS_LBL_VOLUNTEER_SAVED'),'success');
-
-		return true;
-	}
-
-
-	public function onBeforeSave()
-	{
-		// Check if volunteer is editing own profile
-		$item = FOFModel::getTmpInstance('Volunteers', 'VolunteersModel')
-			->user_id(JFactory::getUser()->id)
-			->getFirstItem();
-
-		$volunteer_id 		= $item->volunteers_volunteer_id;
-		$volunteer_edit_id 	= FOFInput::getVar('volunteers_volunteer_id', null, $this->input);
-
-		if($volunteer_id !== $volunteer_edit_id) {
-			$this->setRedirect(JRoute::_('index.php?option=com_volunteers&view=my'), JText::_('You try to edit someone else profile'),'error');
-			return false;
-		}
-
-		$userid = FOFInput::getInt('user_id', null, $this->input);
-
-		if((!$userid) || ($userid == 0)) {
-			$this->input->set('user_id',JFactory::getUser()->id);
-		}
-
-		$file = JRequest::getVar('image', '', 'files', 'array');
-
-		if(!$file['error']) {
-			$rand 		= substr(md5(microtime()),rand(0,26),4);
-			$firstname 	= FOFInput::getVar('firstname', null, $this->input);
-			$lastname 	= FOFInput::getVar('lastname', null, $this->input);
-			$name 		= JFilterOutput::stringURLSafe($firstname.'-'.$lastname);
-			$name		= $rand.'_'.$name;
-
-			// Filetype
-			$filename = ($file['name']);
-			$filetype = strstr($filename, '.');
-			$filename = $name.$filetype;
-
-			// Path
-			$filepath = JPath::clean(JPATH_SITE.'/images/volunteers/'.$filename);
-
-			// Do the upload
-			jimport('joomla.filesystem.file');
-			if (!JFile::upload($file['tmp_name'], $filepath)) {
-				$this->setError(JText::_('Upload file error'));
-				return false;
-			}
-
-			// Instantiate our JImage object
-			$image = new JImage($filepath);
-
-			$sizes = array('250x250', '50x50');
-			$image->createThumbs($sizes, JImage::CROP_RESIZE);
-
-			// Set image url
-			$this->input->set('image', $filename);
-		}
 
 		return true;
 	}

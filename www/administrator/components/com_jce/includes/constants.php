a26fd9580832b133dd1adaa426146d097efdda49
Sander Potjer <github@sanderpotjer.nl>
2018-01-30 22:41:22 +0100

    JCE Update

--- www/administrator/components/com_jce/includes/constants.php
+++ www/administrator/components/com_jce/includes/constants.php
@@ -14,7 +14,7 @@ defined('_JEXEC') or die('RESTRICTED');
 define('WF_JOOMLA15', version_compare(JVERSION, '1.6', '<'));
 
 // Some shortcuts to make life easier
-define('WF_VERSION', '2.6.23');
+define('WF_VERSION', '2.6.24');
 
 // JCE Administration Component
 define('WF_ADMINISTRATOR',     JPATH_ADMINISTRATOR.'/components/com_jce');

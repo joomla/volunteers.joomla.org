52937ac225f3c4b98e78c2274e9b41a724f915da
Sander Potjer <github@sanderpotjer.nl>
2020-04-22 19:53:34 +0200

    JCE 2.8.10

--- www/components/com_jce/editor/tiny_mce/plugins/ui/editor_plugin.js
+++ www/components/com_jce/editor/tiny_mce/plugins/ui/editor_plugin.js
@@ -1,2 +1,2 @@
-/* jce - 2.8.9 | 2020-02-29 | https://www.joomlacontenteditor.net | Copyright (C) 2006 - 2020 Ryan Demmer. All rights reserved | GNU/GPL Version 2 or later - http://www.gnu.org/licenses/gpl-2.0.html */
+/* jce - 2.8.10 | 2020-04-02 | https://www.joomlacontenteditor.net | Copyright (C) 2006 - 2020 Ryan Demmer. All rights reserved | GNU/GPL Version 2 or later - http://www.gnu.org/licenses/gpl-2.0.html */
 !function(){tinymce.create("tinymce.plugins.UiPlugin",{init:function(ed,url){}}),tinymce.PluginManager.add("ui",tinymce.plugins.UiPlugin)}();

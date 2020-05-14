0e431a883dea672af6ea54448bc61c0bad038ea2
Sander Potjer <github@sanderpotjer.nl>
2020-05-15 00:12:18 +0200

    Cookiescript updates

--- www/templates/joomla/index.php
+++ www/templates/joomla/index.php
@@ -167,7 +167,6 @@ if (!$this->getMetaData('referrer'))
 }
 
 // Get the site config
-/** @var JoomlaTemplateHelper $siteConfig */
 $siteConfig = JoomlaTemplateHelper::getSiteConfig(Uri::getInstance()->toString(['host']));
 
 // If Cookie Control is enabled, we expose the GTM ID as a JavaScript var versus registering GTM directly
@@ -175,6 +174,7 @@ $hasCookieControl = $this->params->get('cookieControlActive', 0);
 
 if ($hasCookieControl)
 {
+	HTMLHelper::_('script', 'https://cc.cdn.civiccomputing.com/8/cookieControl-8.x.min.js');
 	HTMLHelper::_('script', 'cookiecontrol.js', ['version' => 'auto', 'relative' => true, 'detectDebug' => (bool) JDEBUG], []);
 
 	// Purposefully declare a global variable versus using the Joomla.options JavaScript API for compatibility with non-Joomla (CMS) installations
@@ -236,11 +236,6 @@ JS
 	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo $siteConfig->gtmId; ?>" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','<?php echo $siteConfig->gtmId; ?>');</script>
 	<!-- End Google Tag Manager -->
-	<?php endif; ?>
-	<?php
-	// Add CookieControl if activated
-	if ($hasCookieControl): ?>
-		<script src="//cc.cdn.civiccomputing.com/8/cookieControl-8.x.min.js" type="text/javascript"></script>
 	<?php endif; ?>
 	<!-- Top Nav -->
 	<nav class="navigation" role="navigation">

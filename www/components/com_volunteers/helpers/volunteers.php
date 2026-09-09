1d10244b0a9df7627c3b5ad0a119ea4c72047de6
George Wilson <georgejameswilson@googlemail.com>
2026-09-09 22:33:12 +0100

    Match J3 width

--- www/components/com_volunteers/helpers/volunteers.php
+++ www/components/com_volunteers/helpers/volunteers.php
@@ -54,7 +54,7 @@ abstract class VolunteersHelper
 		}
 		else
 		{
-			$html = '<img class="img-rounded w-100" alt="' . $alt . '" src="' . $image . '"/>';
+			$html = '<img class="img-rounded mw-100" alt="' . $alt . '" src="' . $image . '"/>';
 		}
 
 		return $html;

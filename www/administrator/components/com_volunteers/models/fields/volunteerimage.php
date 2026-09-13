03a5eeeeb49f2dcfe283202a41f52fa426557c2d
George Wilson <georgejameswilson@googlemail.com>
2026-09-13 22:41:02 +0100

    Remove perfect image plugin usage

---
+++ www/administrator/components/com_volunteers/models/fields/volunteerimage.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * @package		Perfect Image Form Field
+ * @copyright	Copyright (c) 2016 Perfect Web Team / perfectwebteam.nl
+ * @license		GNU General Public License version 3 or later
+ */
+
+// No direct access.
+defined('_JEXEC') or die;
+
+/**
+ * Sample list form field
+ */
+class JFormFieldVolunteerimage extends JFormField
+{
+    /**
+     * The form field type.
+     *
+     * @var  string
+     */
+    protected $type = 'Volunteerimage';
+
+    protected function getInput()
+    {
+        // Setup variables for display.
+        $html = array();
+
+        // Container
+        $html[] = '<div id="' . $this->id . '">';
+
+        // Image
+        $html[] = '<div class="image-preview">';
+
+        if ($this->value)
+        {
+            $html[] = '<img src="' . $this->value . '"/>';
+        }
+
+        $html[] = '</div>';
+
+        $html[] = '<input type="hidden" id="' . $this->id . '" name="' . $this->name . '" value="' . $this->value . '" />';
+
+        // Container end
+        $html[] = '</div>';
+
+        return implode("\n", $html);
+    }
+}

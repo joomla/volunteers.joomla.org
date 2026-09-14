43a01ae9791cf0cb619251ad211be209101aa4cc
George Wilson <georgejameswilson@googlemail.com>
2026-09-14 22:58:53 +0100

    Remove extra submenu items

--- www/administrator/components/com_volunteers/helpers/volunteers.php
+++ www/administrator/components/com_volunteers/helpers/volunteers.php
@@ -23,52 +23,6 @@ class VolunteersHelper extends JHelperContent
 	public static function addSubmenu($vName = 'volunteers')
 	{
 		JHtmlSidebar::addEntry(
-			JText::_('COM_VOLUNTEERS_TITLE_VOLUNTEERS'),
-			'index.php?option=com_volunteers&view=volunteers',
-			$vName == 'volunteers'
-		);
-
-		JHtmlSidebar::addEntry(
-			JText::_('COM_VOLUNTEERS_TITLE_TEAMS'),
-			'index.php?option=com_volunteers&view=teams',
-			$vName == 'teams'
-		);
-
-		JHtmlSidebar::addEntry(
-			JText::_('COM_VOLUNTEERS_TITLE_ROLES'),
-			'index.php?option=com_volunteers&view=roles',
-			$vName == 'roles'
-		);
-
-		JHtmlSidebar::addEntry(
-			JText::_('COM_VOLUNTEERS_TITLE_MEMBERS'),
-			'index.php?option=com_volunteers&view=members',
-			$vName == 'members'
-		);
-
-		JHtmlSidebar::addEntry(
-			JText::_('COM_VOLUNTEERS_TITLE_REPORTS'),
-			'index.php?option=com_volunteers&view=reports',
-			$vName == 'reports'
-		);
-
-		JHtmlSidebar::addEntry('<hr>', '');
-
-		JHtmlSidebar::addEntry(
-			JText::_('COM_VOLUNTEERS_TITLE_DEPARTMENTS'),
-			'index.php?option=com_volunteers&view=departments',
-			$vName == 'departments'
-		);
-
-		JHtmlSidebar::addEntry(
-			JText::_('COM_VOLUNTEERS_TITLE_POSITIONS'),
-			'index.php?option=com_volunteers&view=positions',
-			$vName == 'positions'
-		);
-
-		JHtmlSidebar::addEntry('<hr>', '');
-
-		JHtmlSidebar::addEntry(
 			JText::_('COM_VOLUNTEERS_TITLE_CONTACT'),
 			'index.php?option=com_volunteers&view=contact',
 			$vName == 'contact'
